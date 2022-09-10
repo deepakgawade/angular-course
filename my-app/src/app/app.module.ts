ad0e606d76a3fb3a2e4169b1d5d3d91809f60b52
deepakgawade <deepakgawade22@gmail.com>
2022-09-10 13:16:59 +0530

    nital set up done

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IgxLinearGaugeModule } from "igniteui-angular-gauges";
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +22,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    HttpClientModule,
+    FormsModule,
+    IgxLinearGaugeModule,
+    MatFormFieldModule, 
+    MatIconModule,
+    MatSelectModule, 
+    MatTabsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
